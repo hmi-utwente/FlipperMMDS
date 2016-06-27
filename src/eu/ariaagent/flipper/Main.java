8c2daacd37d862cabc225842c6569f9b9eff3dc4
Barachia <j.b.vanwaterschoot@gmail.com>
2016-06-27 14:48:52 +0200

    no message

--- src/eu/ariaagent/flipper/Main.java
+++ src/eu/ariaagent/flipper/Main.java
@@ -5,38 +5,21 @@
  */
 package eu.ariaagent.flipper;
 
-import eu.ariaagent.managers.*;
-import java.util.ArrayList;
-import java.util.Collection;
-
 /**
  *
  * @author Siewart
  */
 public class Main {
     
-    private Collection <Manager> managers;
-    private BehaviourManager dm;
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main( String args[] )
     {
-        Main dialogueManagementSystem = new Main();        
-    }
-    
-    public Main(){                
-        //InformationManager im = new InformationManager();
-        //FeedbackManager fm = new FeedbackManager();
-        //StructureManager sm = new StructureManager();
-        this.managers = new ArrayList<>();
-        //managers.add(im);
-        //managers.add(fm);
-        //managers.add(sm);        
-        //dm = new BehaviourManager();
-        dm.startDialogue();
-        
-    }    
+        ManagerController mc = new ManagerController();
+        mc.start();
+    }   
     
 }
